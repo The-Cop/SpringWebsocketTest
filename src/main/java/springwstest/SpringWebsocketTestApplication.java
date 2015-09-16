0f259f10ae2f1b5b6fc52db6c4f198dfb6177592
TheCop <kap-kirill@yandex.ru>
2015-09-16 11:31:12 +0300

    initial commit

---
+++ src/main/java/springwstest/SpringWebsocketTestApplication.java
@@ -0,0 +1,12 @@
+package springwstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebsocketTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebsocketTestApplication.class, args);
+    }
+}
